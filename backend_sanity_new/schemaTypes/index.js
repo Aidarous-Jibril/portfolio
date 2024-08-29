3a2d6d2a8da1621dd69fae7513b78bea09c9b6e7
Aidarous Jibril <aidarousj7@gmail.com>
2024-08-29 22:57:54 +0200

    Add new backend_sanity directory

---
+++ backend_sanity_new/schemaTypes/index.js
@@ -0,0 +1,20 @@
+import testimonials from './testimonials'; // Import the testimonials schema
+import abouts from './abouts'; // Import the testimonials schema
+import contact from './contact'; // Import the testimonials schema
+import skills from './skills'; // Import the testimonials schema
+import experiences from './experiences'; // Import the testimonials schema
+import works from './works'; // Import the testimonials schema
+import workExperience from './workExperience'; // Import the testimonials schema
+
+const schemaTypes = [
+    testimonials, 
+    abouts,
+    contact,
+    experiences,
+    skills,
+    works,
+    workExperience
+];
+
+export default schemaTypes;
+
